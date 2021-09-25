a5bfcc98cc85730f4469dfa426392e2bfd640452
Patrick Murphy <murphy.patrick@hotmail.com>
2021-09-25 10:01:59 -0500

    Folder Restructuring

--- 05_Non_ESU_Hotfixes/06_Windows6.1-KB4072650-x64.cmd
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0"
-dism /online /add-package /packagepath:bin\06_Windows6.1-KB4072650-x64.cab /NoRestart
-pause
--- 05_Non_ESU_Hotfixes/06_Windows6.1-KB4072650-x86.cmd
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0"
-dism /online /add-package /packagepath:bin\06_Windows6.1-KB4072650-x86.cab /NoRestart
-pause
