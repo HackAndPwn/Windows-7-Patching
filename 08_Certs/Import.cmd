f54c32a41221af758fa2048c01010771d39b5465
TheBinaryShadow <100244536+TheBinaryShadow@users.noreply.github.com>
2026-03-08 11:35:18 +0100

    Request admin privileges if not ran as admin

--- 08_Certs/Import.cmd
+++ 08_Certs/Import.cmd
@@ -1,3 +1,12 @@
+REM Check for administrator privileges and self-elevate if needed
+
+fsutil dirty query %systemdrive% >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrator privileges...
+    mshta "javascript:var shell=new ActiveXObject('Shell.Application');shell.ShellExecute('%~s0','','','runas',1);close();"
+    exit /b
+)
+
 cd /D "%~dp0"
 for /F "tokens=*" %%i in ('dir /B *.cr*') do (
   %SystemRoot%\System32\certutil.exe -f -addstore Root "%%i"
